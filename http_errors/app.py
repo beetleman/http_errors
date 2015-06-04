ac4b26279fac3dd86dd5948fe8737a43a8c01c8f
beetleman <mateusz.probachta@gmail.com>
2015-06-04 13:14:23 +0200

    add `build` and `create` command.

--- http_errors/app.py
+++ http_errors/app.py
@@ -34,19 +34,33 @@ class App:
             default=False
         )
 
+    def _check(self, project):
+        errors = project.validate()
+        for e in errors:
+            print('error: %s' % e, file=sys.stderr)
+        return len(errors) == 0
+
     def check(self, path, force):
         if force:
             print('`--force` option is useless whith `check`')
         project = Project(path)
-        errors = project.validate()
-        for e in errors:
-            print('error: %s' % e, file=sys.stderr)
+        self._check(project)
 
     def build(self, path, force):
-        print('build(self, path, force)', path, force)
+        project = Project(path)
+        if not self._check(project):
+            return
+        project.build(force)
 
     def create(self, path, force):
-        print('create(self, path, force)', path, force)
+        if force:
+            project = Project(path, force)
+        else:
+            project = Project(path, force)
+            project.create()
+            project.load()
+        if not self._check(project):
+            return
 
     def run(self):
         '''Run app'''
